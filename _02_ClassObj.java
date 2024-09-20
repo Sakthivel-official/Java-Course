810b6306e05697031dfb8241ceef470061218d5e
Sakthi_Official <sakthiveld479@gmail.com>
2024-09-20 18:02:40 +0530

    Date: 16/09/2024

--- _02_ClassObj.java
+++ _02_ClassObj.java
@@ -1,5 +1,5 @@
 
-public class ClassObj {
+public class _02_ClassObj {
 
 	String name="malar";
 	int id=5;
@@ -9,7 +9,7 @@ public class ClassObj {
 	
 	public static void main(String[] args) {
 		
-		ClassObj obj=new ClassObj();
+		_02_ClassObj obj=new _02_ClassObj();   // new - creates a object for ClassObj Class
 		System.out.println(obj.id); 
 		System.out.println(obj.name); 
 		System.out.println(obj.salary); 
