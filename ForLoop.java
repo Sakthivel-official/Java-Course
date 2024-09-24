2441b3a7a9eacffebed1a2b8db8782dd54f464f5
Sakthivel_D <sakthiveld479@gmail.com>
2024-09-24 20:40:22 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		//for(initialization; condition; increment/decrement)
+		
+		for(int i=0;i<=5;i++) {
+			if(i%2 == 0) {  //conditon to print even numbers
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
