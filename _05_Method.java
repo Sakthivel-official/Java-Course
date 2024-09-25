6bbeffaa62aace227a4f50aaca6303ad59019069
Sakthivel_D <sakthiveld479@gmail.com>
2024-09-25 21:46:37 +0530

    Date: 24/09/2024

--- _05_Method.java
+++ _05_Method.java
@@ -1,15 +1,14 @@
 
-public class Method {
+public class Method { // A method is a block of code which only runs when it is called
+		      // To reuse code: define the code once & use it many times
 	static void display(String name) {
 		System.out.println(name+" :is my course");
 	}
 
 	public static void main(String[] args) {
-		
 		Method.display("c");
 		Method.display("Java");
 		Method.display("Python");
-		
 	}
 
 }
